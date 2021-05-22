c26f5d945b9e6cfae086cb60c9641be74875da95
Min <mtan121@aucklanduni.ac.nz>
2021-05-22 17:44:27 +0800

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name)
+    }
+
+function appendDriver(name){
+    return[...drivers, name]
+}
+
+function destructivelyPrependDriver(name){
+    drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver(name){
+    drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver(name){
+    drivers.shift()
+}
+
+function prependDriver(name){
+    return[name, ...drivers]
+}
+
+function removeLastDriver(){
+    const copyDrivers = [... drivers]
+    copyDrivers.pop()
+    return copyDrivers
+}
+
+function removeFirstDriver(){
+    const copyDrivers = [... drivers]
+    copyDrivers.shift()
+    return copyDrivers
+}
